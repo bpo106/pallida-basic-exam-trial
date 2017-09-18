922e4dde0bcc9fd6b0ffad8f53f9aaf67068c25e
bpo106 <bpo106bkv@gmail.com>
2017-09-18 14:38:05 +0200

    OddFilter is done.

---
+++ oddfilter/oddfilter/oddfilter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddFilter
+{
+    class Program
+    {
+        static List<int> OddFilter (List<int> oddElements)
+        {
+            for ( int i = 0 ; i < oddElements.Count ; i++ )
+            {
+                if (oddElements[i] % 2 == 0)
+                {
+                    oddElements.Remove(oddElements[i]);
+                }
+            }
+
+            return oddElements;
+        }
+
+        private static void PrintList(List<int> list)
+        {
+            foreach (int element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Create an OddFilter function that takes a list as a parameter,
+            // and returns a new list with every odd element from the orignal list
+            List<int> oddElements = OddFilter(new List<int> { 1, 2, 3, 4, 5 });
+
+            // In case of the example input above, the given PrintList function should print 1 3 5 
+            PrintList(oddElements);
+
+            Console.ReadLine();
+        }
+    }
+}
